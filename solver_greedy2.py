50d2d75d3da057e41f1594946fc883a89234475a
IkukoHiraga <Ikuko@Ikuko-H.local>
2016-07-08 16:55:55 +0900

    second push

---
+++ solver_greedy2.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import sys
+import math
+
+from common import print_solution, read_input
+
+
+def distance(city1, city2):
+    return math.sqrt((city1[0] - city2[0]) ** 2 + (city1[1] - city2[1]) ** 2)
+
+
+
+def opt_2(size, path,dist):
+    total = 0
+    while True:
+        count = 0
+        for i in xrange(size - 2):
+            i1 = i + 1
+            for j in xrange(i + 2, size):
+                if j == size - 1:
+                    j1 = 0
+                else:
+                    j1 = j + 1
+                if i != 0 or j1 != 0:
+                    l1 = dist[path[i]][path[i1]]
+                    l2 = dist[path[j]][path[j1]]
+                    l3 = dist[path[i]][path[j]]
+                    l4 = dist[path[i1]][path[j1]]
+                    if l1 + l2 > l3 + l4:
+                        #reconnect
+                        new_path = path[i1:j+1]
+                        path[i1:j+1] = new_path[::-1]
+                        count += 1
+        total += count
+        if count == 0: break
+    return path
+
+def solve(cities):
+    N = len(cities)
+    adj = N*[0]
+
+    dist = [[0] * N for i in range(N)]
+    dist2 = [[0] * N for i in range(N)]
+    for i in range(N):
+        for j in range(N):
+            dist2[i][j] = dist2[j][i] = distance(cities[i], cities[j])
+
+
+    for i in range(N):
+        adj[i]=0
+        for j in range(N):
+            adj[i]+=distance(cities[i],cities[j])
+            
+        adj[i]=adj[i]/(N-1)
+
+    for i in range(N):
+        for j in range(N):
+            dist[i][j] = distance(cities[i],cities[j])-(adj[i]+adj[j]) 
+            dist[j][i] = distance(cities[j],cities[i])-(adj[j]+adj[i])
+
+    current_city = 0
+    unvisited_cities = set(range(1, N))
+    solution = [current_city]
+
+
+
+
+    def distance_from_current_city(to):
+        return dist[current_city][to]
+
+
+    while unvisited_cities:
+        next_city = min(unvisited_cities, key=distance_from_current_city)
+        unvisited_cities.remove(next_city)
+        solution.append(next_city)
+        current_city = next_city
+
+    solution=opt_2(N,solution,dist2)
+
+    return solution
+
+
+
+def sum_distance(solution,cities):
+    i = 0
+    sum_d = 0.0
+    for i in range(len(solution)-1):
+        sum_d += distance(cities[solution[i]],cities[solution[i+1]])
+    sum_d +=distance(cities[solution[i+1]],cities[solution[0]])
+    return sum_d
+
+
+if __name__ == '__main__':
+    assert len(sys.argv) > 1
+    solution = solve(read_input(sys.argv[1]))
+
+    print sum_distance(solution,read_input(sys.argv[1]))
--- solver_mine_0.py
+++ solver_mine_0.py
@@ -14,6 +14,7 @@ def distance(city1, city2):
 def sum_distance(solution,cities):
     i = 0
     sum_d = 0.0
+    print solution
     for i in range(len(solution)-1):
         sum_d += distance(cities[solution[i]-1],cities[solution[i+1]-1])
     sum_d +=distance(cities[solution[i+1]-1],cities[solution[0]-1])
@@ -24,16 +25,26 @@ def sum_distance(solution,cities):
 def solve(cities):
     # Build a trivial solution.
     # Visit the cities in the order they appear in the input.
-    l = [1, 2, 3, 4, 5]
+    l=[]
+    n=5
+    k=1
+    for k in range(1,n+1):
+        l.append(k)
+
     elements = []
-    min_dis=5000.0
-    for element in itertools.permutations(l, 5):
+    min_dis=500000.0
+    for element in itertools.permutations(l, n):
         elements.append(element)
 
+
+
+
     j=0
-    for j in range(len(elements)):
+    for j in range(len(elements)/n):
+        print j
         if min_dis>sum_distance(elements[j],cities):
             min_dis=sum_distance(elements[j],cities)
+
     return min_dis
         
 
